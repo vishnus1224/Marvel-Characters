939a1c89009d512d886c556ce24e27d3d5d79265
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-28 22:04:35 +0530

    created base activity

---
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.vishnus1224.marvelcharacters.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Base activity to be extended by all activities in the application.
+ * Created by Vishnu on 4/28/2016.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
