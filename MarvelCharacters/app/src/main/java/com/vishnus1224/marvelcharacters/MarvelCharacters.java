673bae45c6abb80a148d12ddda28c094c74f5f41
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-28 22:30:19 +0530

    created a custom application class and set its reference in the manifest

---
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/MarvelCharacters.java
@@ -0,0 +1,15 @@
+package com.vishnus1224.marvelcharacters;
+
+import android.app.Application;
+
+/**
+ * Custom application class.
+ * Created by Vishnu on 4/28/2016.
+ */
+public class MarvelCharacters extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
