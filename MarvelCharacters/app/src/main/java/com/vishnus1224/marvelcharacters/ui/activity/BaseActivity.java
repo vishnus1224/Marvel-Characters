47272e0d9a9366c3c83666269ccadaa5d3845df4
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-28 23:03:32 +0530

    getters for the application and the application component

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/activity/BaseActivity.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/activity/BaseActivity.java
@@ -2,9 +2,30 @@ package com.vishnus1224.marvelcharacters.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 
+import com.vishnus1224.marvelcharacters.MarvelCharacters;
+import com.vishnus1224.marvelcharacters.di.component.ApplicationComponent;
+
 /**
  * Base activity to be extended by all activities in the application.
  * Created by Vishnu on 4/28/2016.
  */
 public class BaseActivity extends AppCompatActivity {
+
+    /**
+     * Get the Application instance.
+     * @return MarvelCharacters instance.
+     */
+    protected MarvelCharacters getMarvelCharacters(){
+        return (MarvelCharacters) getApplication();
+    }
+
+    /**
+     * Get the application component from the application class for providing it to dependent components.
+     * @return ApplicationComponent instance.
+     */
+    protected ApplicationComponent getApplicationComponent(){
+        return getMarvelCharacters().getApplicationComponent();
+    }
+
+
 }
