91f31def524eec492adb529c4a26832a18dc431c
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-28 22:53:39 +0530

    added dependency for the net module

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/di/component/ApplicationComponent.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/di/component/ApplicationComponent.java
@@ -2,6 +2,7 @@ package com.vishnus1224.marvelcharacters.di.component;
 
 import com.vishnus1224.marvelcharacters.MarvelCharacters;
 import com.vishnus1224.marvelcharacters.di.module.ApplicationModule;
+import com.vishnus1224.marvelcharacters.di.module.NetModule;
 
 import javax.inject.Singleton;
 
@@ -11,7 +12,7 @@ import dagger.Component;
  * Created by Vishnu on 4/28/2016.
  */
 @Singleton
-@Component(modules = ApplicationModule.class)
+@Component(modules = {ApplicationModule.class, NetModule.class})
 public interface ApplicationComponent {
 
     /**
