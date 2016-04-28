202640e0e5166b607414e1f45f6983c07e1eef17
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-28 22:54:09 +0530

    web service class for accessing the marvel api

---
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/webservice/MarvelWebService.java
@@ -0,0 +1,9 @@
+package com.vishnus1224.marvelcharacters.webservice;
+
+/**
+ * Created by Vishnu on 4/28/2016.
+ */
+public interface MarvelWebService {
+
+    String BASE_URL = "https://gateway.marvel.com/";
+}
