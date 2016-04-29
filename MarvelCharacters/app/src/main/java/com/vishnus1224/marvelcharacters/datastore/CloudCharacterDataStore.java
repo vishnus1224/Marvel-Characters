84164d4082062055f438ce42b727c8a88d6f01d9
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-29 12:36:44 +0530

    called the web service method for fetching data

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/datastore/CloudCharacterDataStore.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/datastore/CloudCharacterDataStore.java
@@ -30,6 +30,6 @@ public class CloudCharacterDataStore implements CharacterDataStore {
 
     @Override
     public Observable<List<MarvelCharacter>> fetchMarvelCharacters() {
-        return null;
+        return marvelWebService.fetchMarvelCharacters();
     }
 }
