a7f48350edffbb44c48605f7cb43cff3f9d2c019
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-29 14:59:26 +0530

    made the class parcelable

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/model/Comic.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/model/Comic.java
@@ -1,13 +1,33 @@
 package com.vishnus1224.marvelcharacters.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Vishnu on 4/29/2016.
  */
-public class Comic {
+public class Comic implements Parcelable {
 
     private String resourceURI;
 
     private String name;
+
+    protected Comic(Parcel in) {
+        resourceURI = in.readString();
+        name = in.readString();
+    }
+
+    public static final Creator<Comic> CREATOR = new Creator<Comic>() {
+        @Override
+        public Comic createFromParcel(Parcel in) {
+            return new Comic(in);
+        }
+
+        @Override
+        public Comic[] newArray(int size) {
+            return new Comic[size];
+        }
+    };
 
     public String getResourceURI() {
         return resourceURI;
@@ -23,5 +43,16 @@ public class Comic {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(resourceURI);
+        parcel.writeString(name);
     }
 }
