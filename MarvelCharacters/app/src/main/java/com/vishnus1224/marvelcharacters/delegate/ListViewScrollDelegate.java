fd8695a43a3868e63a32908de4fe626c1337ef26
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-29 19:01:49 +0530

    implementation of OnScrollListener for listening to the listview bottom reached event

---
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/delegate/ListViewScrollDelegate.java
@@ -0,0 +1,81 @@
+package com.vishnus1224.marvelcharacters.delegate;
+
+import android.widget.AbsListView;
+
+/**
+ * Delegate to detect if the list view has scrolled to its bottom.
+ * Created by Vishnu on 4/29/2016.
+ */
+public class ListViewScrollDelegate implements AbsListView.OnScrollListener {
+
+    private BottomHitListener bottomHitListener;
+
+    /**
+     * The position of the visible item. Used to prevent multiple calls to onBottomHit.
+     */
+    private int lastItemPosition;
+
+    /**
+     * The offset from the last item at which the onBottomHit method will fire.
+     * The onBottomHit method will be called when the list view has scrolled past the third last item.
+     */
+    private int lastItemOffset = 2;
+
+    public ListViewScrollDelegate(BottomHitListener bottomHitListener){
+
+        this.bottomHitListener = bottomHitListener;
+
+    }
+
+    /**
+     * Reset the last item position. Will be used if an error occurs while fetching data.
+     * If it is not reset, then the delegate will no signal that scroll has reached its bottom.
+     */
+    public void resetLastItemPosition(){
+
+        lastItemPosition = 0;
+
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+        int currentPosition = firstVisibleItem + visibleItemCount;
+
+        //calculate the offset position from the specified offset.
+        int offsetPosition = totalItemCount - lastItemOffset;
+
+        if(currentPosition == offsetPosition && totalItemCount > 0){
+
+            if(lastItemPosition != currentPosition) {
+
+                lastItemPosition = currentPosition;
+
+                if (bottomHitListener != null) {
+
+                    bottomHitListener.onBottomHit();
+
+                }
+
+            }
+
+        }
+    }
+
+    /**
+     * Notifies the calling the class that the list view has reached its bottom.
+     */
+    public interface BottomHitListener {
+
+        /**
+         * ListView has hit its bottom.
+         */
+        void onBottomHit();
+
+    }
+}
