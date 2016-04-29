d6033d4331ec940fa449b24630b8380857058c80
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-29 15:49:18 +0530

    model for marvel character

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/model/MarvelCharacter.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/model/MarvelCharacter.java
@@ -1,26 +1,73 @@
 package com.vishnus1224.marvelcharacters.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Vishnu on 4/29/2016.
  */
-public class MarvelCharacter extends BaseModel {
+public class MarvelCharacter {
 
+    /**
+     * Character id.
+     */
+    private int id;
+
+    /**
+     * Character name.
+     */
     private String name;
-    private String imageURL;
+
+    /**
+     * Character description.
+     */
+    private String description;
+
+    /**
+     * Character thumbnail image.
+     */
+    private MarvelCharacterThumbnail thumbnail;
+
+    @SerializedName("comics")
+    private ComicContainer comicContainer;
+
+    @SerializedName("stories")
+    private StoryContainer storyContainer;
+
+    @SerializedName("events")
+    private EventContainer eventContainer;
+
+    @SerializedName("series")
+    private SeriesContainer seriesContainer;
+
+    public int getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getDescription() {
+        return description;
     }
 
-    public String getImageURL() {
-        return imageURL;
+    public MarvelCharacterThumbnail getThumbnail() {
+        return thumbnail;
     }
 
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
+    public ComicContainer getComicContainer() {
+        return comicContainer;
+    }
+
+    public StoryContainer getStoryContainer() {
+        return storyContainer;
+    }
+
+    public EventContainer getEventContainer() {
+        return eventContainer;
+    }
+
+    public SeriesContainer getSeriesContainer() {
+        return seriesContainer;
     }
 }
