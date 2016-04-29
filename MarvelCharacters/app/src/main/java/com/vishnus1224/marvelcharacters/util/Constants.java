82273fd8de42f2342d4f3441828abd583ed2cb74
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-29 13:13:32 +0530

    created constants for storing constant values

---
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/util/Constants.java
@@ -0,0 +1,10 @@
+package com.vishnus1224.marvelcharacters.util;
+
+/**
+ * Created by Vishnu on 4/29/2016.
+ */
+public class Constants {
+
+    public static final String ZZX = "f511715ab63c4c5720d33417f6c20282";
+    public static final String ZZY = "e66f12edcfa3637f01f84dc4ec5128248a682e6d";
+}
