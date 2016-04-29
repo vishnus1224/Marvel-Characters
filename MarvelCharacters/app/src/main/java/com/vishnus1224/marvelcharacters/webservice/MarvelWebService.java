b22663e0788c7e01e38dde3183f5fa87908bca6f
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-29 12:36:25 +0530

    implementation of the web service method for getting a list of marvel characters from the server

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/webservice/MarvelWebService.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/webservice/MarvelWebService.java
@@ -1,9 +1,19 @@
 package com.vishnus1224.marvelcharacters.webservice;
 
+import com.vishnus1224.marvelcharacters.model.MarvelCharacter;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import rx.Observable;
+
 /**
  * Created by Vishnu on 4/28/2016.
  */
 public interface MarvelWebService {
 
     String BASE_URL = "https://gateway.marvel.com/";
+
+    @GET("v1/public/characters")
+    Observable<List<MarvelCharacter>> fetchMarvelCharacters();
 }
