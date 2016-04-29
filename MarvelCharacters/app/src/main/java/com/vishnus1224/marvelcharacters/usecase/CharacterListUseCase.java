64164c115c2b0c792a30c5146409999c9137b30f
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-29 12:19:57 +0530

    character list use case implementation for fetching a list of marvel characters

---
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/usecase/CharacterListUseCase.java
@@ -0,0 +1,34 @@
+package com.vishnus1224.marvelcharacters.usecase;
+
+import com.vishnus1224.marvelcharacters.repository.CharacterRepository;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+
+/**
+ * Created by Vishnu on 4/29/2016.
+ */
+public class CharacterListUseCase extends UseCase {
+
+    /**
+     * Reference to the character repository.
+     */
+    private CharacterRepository characterRepository;
+
+    @Inject
+    public CharacterListUseCase(CharacterRepository characterRepository){
+
+        this.characterRepository = characterRepository;
+
+    }
+
+    /**
+     * Fetch a list of characters from the repository.
+     * @return Observable emitting a list of characters.
+     */
+    @Override
+    protected Observable buildUseCase() {
+        return characterRepository.fetchMarvelCharacters();
+    }
+}
