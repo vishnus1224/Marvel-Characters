6aa8b2c96e4ce6077f59a4730582cfe1261e8801
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-29 15:50:33 +0530

    added data mapper to transform data into the required format

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/repository/CharacterRepositoryImpl.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/repository/CharacterRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.vishnus1224.marvelcharacters.repository;
 
 import com.vishnus1224.marvelcharacters.datastore.CharacterDataStore;
+import com.vishnus1224.marvelcharacters.mapper.CharacterDataMapper;
+import com.vishnus1224.marvelcharacters.model.CharacterDataWrapper;
 import com.vishnus1224.marvelcharacters.model.MarvelCharacter;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 /**
  * Implementation of the CharacterRepository for getting a list of MarvelCharacters.
@@ -21,15 +24,34 @@ public class CharacterRepositoryImpl implements CharacterRepository {
      */
     private CharacterDataStore characterDataStore;
 
+    /**
+     * Mapper for transforming data.
+     */
+    private CharacterDataMapper characterDataMapper;
+
     @Inject
-    public CharacterRepositoryImpl(@Named("Cloud") CharacterDataStore characterDataStore){
+    public CharacterRepositoryImpl(@Named("Cloud") CharacterDataStore characterDataStore, CharacterDataMapper characterDataMapper){
 
         this.characterDataStore = characterDataStore;
 
+        this.characterDataMapper = characterDataMapper;
+
     }
 
+    /**
+     * Obtain a list of marvel characters by transforming the data using data mapper.
+     * @return List of marvel characters.
+     */
     @Override
     public Observable<List<MarvelCharacter>> fetchMarvelCharacters() {
-        return characterDataStore.fetchMarvelCharacters();
+        return characterDataStore.fetchMarvelCharacters()
+                .map(new Func1<CharacterDataWrapper, List<MarvelCharacter>>() {
+                    @Override
+                    public List<MarvelCharacter> call(CharacterDataWrapper characterDataWrapper) {
+
+                        return characterDataMapper.unwrapMarvelCharacters(characterDataWrapper);
+
+                    }
+                });
     }
 }
