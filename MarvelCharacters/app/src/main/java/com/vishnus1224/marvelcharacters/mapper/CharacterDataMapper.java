d7ff1d9e27dfd9a363b321a7043e5aea97fe6b6b
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-30 15:24:12 +0530

    added method to transform data to a cursor

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/mapper/CharacterDataMapper.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/mapper/CharacterDataMapper.java
@@ -1,7 +1,11 @@
 package com.vishnus1224.marvelcharacters.mapper;
 
+import android.database.Cursor;
+import android.database.MatrixCursor;
+
 import com.vishnus1224.marvelcharacters.model.CharacterDataWrapper;
 import com.vishnus1224.marvelcharacters.model.MarvelCharacter;
+import com.vishnus1224.marvelcharacters.util.Constants;
 
 import java.util.List;
 
@@ -28,5 +32,47 @@ public class CharacterDataMapper {
 
         return characterDataWrapper.getCharacterDataHolder().getMarvelCharacters();
 
+    }
+
+    /**
+     * Transforms data to a cursor.
+     * @param characterDataWrapper Data to be transformed.
+     * @return Cursor with the mapped data.
+     */
+    public Cursor transformToCursor(CharacterDataWrapper characterDataWrapper){
+
+        List<MarvelCharacter> characterList = unwrapMarvelCharacters(characterDataWrapper);
+
+        int characterListSize = characterList.size();
+
+        MatrixCursor matrixCursor = new MatrixCursor(Constants.SUGGESTIONS_ADAPTER_COLUMNS, characterListSize);
+
+        for(int i = 0; i < characterListSize; i++){
+
+            MarvelCharacter marvelCharacter = characterList.get(i);
+
+            matrixCursor.addRow(makeDatabaseRow(marvelCharacter));
+
+        }
+
+        return matrixCursor;
+    }
+
+    /**
+     * Create a row to be added to the cursor.
+     * @param marvelCharacter Character holding the data.
+     * @return Cursor row with String data.
+     */
+    private String[] makeDatabaseRow(MarvelCharacter marvelCharacter) {
+
+        String[] row = new String[3];
+
+        row[0] = String.valueOf(marvelCharacter.getId());
+
+        row[1] = marvelCharacter.getName();
+
+        row[2] = marvelCharacter.getThumbnail().getFinalPath();
+
+        return row;
     }
 }
