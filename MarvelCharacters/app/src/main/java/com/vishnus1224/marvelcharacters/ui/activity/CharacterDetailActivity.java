748b7ed4cc5e9837ca6170feb2f08c5d706bda04
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-30 19:15:43 +0530

    setup the views

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/activity/CharacterDetailActivity.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/activity/CharacterDetailActivity.java
@@ -2,6 +2,10 @@ package com.vishnus1224.marvelcharacters.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.vishnus1224.marvelcharacters.R;
 import com.vishnus1224.marvelcharacters.model.MarvelCharacter;
@@ -13,6 +17,33 @@ import com.vishnus1224.marvelcharacters.util.Constants;
  */
 public class CharacterDetailActivity extends BaseActivity {
 
+    //View declaration.
+    //=========================================
+    private TextView characterDescriptionTextView;
+
+    private TextView comicsTitleTextView;
+    private TextView seriesTitleTextView;
+    private TextView storiesTitleTextView;
+    private TextView eventsTitleTextView;
+
+    private RecyclerView comicsRecyclerView;
+    private RecyclerView seriesRecyclerView;
+    private RecyclerView storiesRecyclerView;
+    private RecyclerView eventsRecyclerView;
+
+    private ListView relatedLinksListView;
+
+    //=========================================
+    //View declaration end.
+
+    private RecyclerView.LayoutManager layoutManager;
+
+    private RecyclerView.Adapter comicsAdapter;
+    private RecyclerView.Adapter seriesAdapter;
+    private RecyclerView.Adapter storiesAdapter;
+    private RecyclerView.Adapter eventsAdapter;
+
+
     private MarvelCharacter marvelCharacter;
 
     @Override
@@ -20,9 +51,36 @@ public class CharacterDetailActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_detail);
 
+        setupViews();
+
         Bundle extras = getIntent().getExtras();
 
         getCharacterFromBundle(extras);
+    }
+
+    private void setupViews() {
+
+        characterDescriptionTextView = (TextView) findViewById(R.id.characterDescription);
+
+        LinearLayout comicsLayout = (LinearLayout) findViewById(R.id.characterComicsLayout);
+        comicsTitleTextView = (TextView) comicsLayout.findViewById(R.id.textViewLayoutTitle);
+        comicsRecyclerView = (RecyclerView) comicsLayout.findViewById(R.id.recyclerViewItems);
+
+        LinearLayout seriesLayout = (LinearLayout) findViewById(R.id.characterSeriesLayout);
+        seriesTitleTextView = (TextView) seriesLayout.findViewById(R.id.textViewLayoutTitle);
+        seriesRecyclerView = (RecyclerView) seriesLayout.findViewById(R.id.recyclerViewItems);
+
+        LinearLayout storiesLayout = (LinearLayout) findViewById(R.id.characterStoriesLayout);
+        storiesTitleTextView = (TextView) storiesLayout.findViewById(R.id.textViewLayoutTitle);
+        seriesRecyclerView = (RecyclerView) storiesLayout.findViewById(R.id.recyclerViewItems);
+
+        LinearLayout eventsLayout = (LinearLayout) findViewById(R.id.characterEventsLayout);
+        eventsTitleTextView = (TextView) eventsLayout.findViewById(R.id.textViewLayoutTitle);
+        eventsRecyclerView = (RecyclerView) eventsLayout.findViewById(R.id.recyclerViewItems);
+
+        LinearLayout relatedLinksLayout = (LinearLayout) findViewById(R.id.characterRelatedLinksLayout);
+        relatedLinksListView = (ListView) relatedLinksLayout.findViewById(R.id.listViewRelatedLinks);
+
     }
 
     private void getCharacterFromBundle(Bundle extras) {
