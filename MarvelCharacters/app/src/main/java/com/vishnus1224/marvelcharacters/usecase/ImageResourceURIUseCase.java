a1ae888a4e691b679d27f810785ef263a7beb993
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-05-01 12:08:35 +0530

    implementation for the use case

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/usecase/ImageResourceURIUseCase.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/usecase/ImageResourceURIUseCase.java
@@ -1,5 +1,9 @@
 package com.vishnus1224.marvelcharacters.usecase;
 
+import com.vishnus1224.marvelcharacters.repository.ImageResourceRepository;
+
+import javax.inject.Inject;
+
 import rx.Observable;
 
 /**
@@ -9,12 +13,23 @@ public class ImageResourceURIUseCase extends UseCase {
 
     private String resourceURI;
 
+    private ImageResourceRepository imageResourceRepository;
+
+    @Inject
+    public ImageResourceURIUseCase(ImageResourceRepository imageResourceRepository){
+
+        this.imageResourceRepository = imageResourceRepository;
+
+    }
+
     public void setResourceURI(String resourceURI){
         this.resourceURI = resourceURI;
     }
 
     @Override
     protected Observable buildUseCase() {
-        return null;
+
+        return imageResourceRepository.getCharacterResourceThumbnail(resourceURI);
+
     }
 }
