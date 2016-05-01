de95b7fb1a1cf10497e1a78022be68fda314488c
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-05-01 15:23:31 +0530

    executed the use case

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/presenter/CharacterDetailPresenter.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/presenter/CharacterDetailPresenter.java
@@ -3,11 +3,19 @@ package com.vishnus1224.marvelcharacters.ui.presenter;
 import android.widget.ImageView;
 
 import com.vishnus1224.marvelcharacters.di.scope.PerActivity;
+import com.vishnus1224.marvelcharacters.model.CharacterResourceThumbnail;
+import com.vishnus1224.marvelcharacters.ui.view.CharacterDetailView;
 import com.vishnus1224.marvelcharacters.usecase.ImageResourceURIUseCase;
 import com.vishnus1224.marvelcharacters.usecase.UseCase;
+import com.vishnus1224.marvelcharacters.util.URLExtractor;
+import com.vishnus1224.marvelcharacters.webservice.MarvelWebService;
+
+import java.lang.ref.WeakReference;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import rx.Subscriber;
 
 /**
  * Created by Vishnu on 5/1/2016.
@@ -17,16 +25,65 @@ public class CharacterDetailPresenter {
 
     private UseCase imageResourceURIUseCase;
 
-    @Inject
-    public CharacterDetailPresenter(@Named("ImageResourceURI") UseCase imageResourceURIUseCase){
+    private URLExtractor urlExtractor;
 
-        this.imageResourceURIUseCase = imageResourceURIUseCase;
+    private CharacterDetailView characterDetailView;
+
+    /**
+     * Initialize the presenter.
+     * @param characterDetailView View with which the presenter will communicate.
+     */
+    public void init(CharacterDetailView characterDetailView){
+        this.characterDetailView = characterDetailView;
+    }
+
+    /**
+     * Perform clean up.
+     */
+    public void destroy(){
+
+        imageResourceURIUseCase.unSubscribe();
 
     }
 
-    public void fetchImageData(String resourceURI, ImageView imageView){
+    @Inject
+    public CharacterDetailPresenter(@Named("ImageResourceURI") UseCase imageResourceURIUseCase, URLExtractor urlExtractor){
 
-        ((ImageResourceURIUseCase)imageResourceURIUseCase).setResourceURI(resourceURI);
+        this.imageResourceURIUseCase = imageResourceURIUseCase;
+
+        this.urlExtractor = urlExtractor;
+
+    }
+
+    public void fetchImageData(String resourceURI, final WeakReference<ImageView> imageViewWeakReference){
+
+        ((ImageResourceURIUseCase)imageResourceURIUseCase).setResourceURI(urlExtractor.removeStringFromURL(resourceURI, MarvelWebService.BASE_URL));
+
+        imageResourceURIUseCase.execute(new Subscriber<CharacterResourceThumbnail>(){
+
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+
+            }
+
+            @Override
+            public void onNext(CharacterResourceThumbnail characterResourceThumbnail) {
+
+                //if the image view stored in the weak reference still exists.
+                if(imageViewWeakReference.get() != null){
+
+                    characterDetailView.loadImage(characterResourceThumbnail.getFinalPath(), imageViewWeakReference.get());
+
+                }
+
+            }
+        });
 
     }
 }
