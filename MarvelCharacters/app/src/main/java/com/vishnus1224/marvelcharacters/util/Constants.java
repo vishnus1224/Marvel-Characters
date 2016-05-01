6cf9612ac0452c5023d713b06965bef2ec96c711
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-05-01 18:12:23 +0530

    added new key

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/util/Constants.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/util/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     public static final String ZZY = "e66f12edcfa3637f01f84dc4ec5128248a682e6d";
 
     public static final String KEY_CHARACTER = "MarvelCharacter";
+    public static final String KEY_RESOURCE_URI_LIST = "ResourceURIList";
 
     public static final String[] SUGGESTIONS_ADAPTER_COLUMNS =
             {"_id", "Name", "Thumb"};
