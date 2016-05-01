2f752062956a42030321429743cfc6409ee29aee
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-05-01 23:15:48 +0530

    saving characters to cache and retrieving a single character by id when needed

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/datastore/CloudCharacterDataStore.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/datastore/CloudCharacterDataStore.java
@@ -1,11 +1,16 @@
 package com.vishnus1224.marvelcharacters.datastore;
 
 import com.vishnus1224.marvelcharacters.api.RESTAPI;
+import com.vishnus1224.marvelcharacters.cache.CharacterCache;
 import com.vishnus1224.marvelcharacters.model.CharacterDataWrapper;
+import com.vishnus1224.marvelcharacters.model.MarvelCharacter;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import rx.Observable;
+import rx.functions.Action1;
 
 /**
  * Implementation of character data store that fetches data from the cloud.
@@ -18,11 +23,17 @@ public class CloudCharacterDataStore implements CharacterDataStore {
      */
     private RESTAPI restApi;
 
+    /**
+     * Cache for storing characters.
+     */
+    private CharacterCache characterCache;
+
     @Inject
-    public CloudCharacterDataStore(RESTAPI restApi){
+    public CloudCharacterDataStore(RESTAPI restApi, CharacterCache characterCache){
 
         this.restApi = restApi;
 
+        this.characterCache = characterCache;
     }
 
 
@@ -33,7 +44,39 @@ public class CloudCharacterDataStore implements CharacterDataStore {
 
     @Override
     public Observable<CharacterDataWrapper> searchMarvelCharacters(String characterName) {
-        return restApi.searchMarvelCharacters(characterName);
+        return restApi.searchMarvelCharacters(characterName)
+                .doOnNext(new Action1<CharacterDataWrapper>() {
+                    @Override
+                    public void call(CharacterDataWrapper characterDataWrapper) {
+
+                        //save characters to cache.
+                        saveCharactersToCache(characterDataWrapper);
+
+                    }
+                });
+    }
+
+    private void saveCharactersToCache(CharacterDataWrapper characterDataWrapper) {
+
+        List<MarvelCharacter> marvelCharacters = characterDataWrapper.getCharacterDataHolder().getMarvelCharacters();
+
+        for(MarvelCharacter marvelCharacter : marvelCharacters){
+
+            characterCache.addEntry(marvelCharacter.getId(), marvelCharacter);
+        }
+
+    }
+
+    @Override
+    public Observable<MarvelCharacter> fetchCharacterByID(int characterID) {
+
+        if(characterCache.containsCharacter(characterID)){
+
+            return Observable.just(characterCache.get(characterID));
+
+        }
+
+        return null;
     }
 
 }
