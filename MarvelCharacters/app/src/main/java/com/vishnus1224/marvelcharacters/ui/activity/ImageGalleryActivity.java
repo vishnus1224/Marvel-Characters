0647fd178741a1758c695565db8cb400456ff763
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-05-01 17:28:51 +0530

    activity for showing image gallery

---
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/activity/ImageGalleryActivity.java
@@ -0,0 +1,15 @@
+package com.vishnus1224.marvelcharacters.ui.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Vishnu on 5/1/2016.
+ */
+public class ImageGalleryActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
