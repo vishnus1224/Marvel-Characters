a3820ea4666b124acd71f0bff017b49b7abfe53b
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-05-01 21:51:49 +0530

    injected dependencies , implemented view methods and setup presenter

--- MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/activity/ImageGalleryActivity.java
+++ MarvelCharacters/app/src/main/java/com/vishnus1224/marvelcharacters/ui/activity/ImageGalleryActivity.java
@@ -4,20 +4,32 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vishnus1224.marvelcharacters.R;
+import com.vishnus1224.marvelcharacters.delegate.ImageLoadDelegate;
+import com.vishnus1224.marvelcharacters.di.component.ActivityComponent;
+import com.vishnus1224.marvelcharacters.di.component.DaggerActivityComponent;
+import com.vishnus1224.marvelcharacters.di.module.ActivityModule;
+import com.vishnus1224.marvelcharacters.imageloader.ImageDownloader;
 import com.vishnus1224.marvelcharacters.model.Summary;
 import com.vishnus1224.marvelcharacters.ui.adapter.GalleryPagerAdapter;
+import com.vishnus1224.marvelcharacters.ui.presenter.GalleryPresenter;
 import com.vishnus1224.marvelcharacters.ui.transformer.ZoomOutPageTransformer;
+import com.vishnus1224.marvelcharacters.ui.view.GalleryView;
 import com.vishnus1224.marvelcharacters.util.Constants;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
+
+import javax.inject.Inject;
 
 /**
  * Created by Vishnu on 5/1/2016.
  */
-public class ImageGalleryActivity extends BaseActivity implements View.OnClickListener {
+public class ImageGalleryActivity extends BaseActivity implements View.OnClickListener, GalleryView, ImageLoadDelegate {
 
     private ImageButton closeButton;
 
@@ -32,6 +44,14 @@ public class ImageGalleryActivity extends BaseActivity implements View.OnClickLi
 
     private int currentPosition;
 
+    @Inject
+    GalleryPresenter galleryPresenter;
+
+    @Inject
+    ImageDownloader imageDownloader;
+
+    private ActivityComponent activityComponent;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,9 +59,13 @@ public class ImageGalleryActivity extends BaseActivity implements View.OnClickLi
 
         setupViews();
 
+        injectActivityComponent();
+
         obtainDataFromBundle();
 
         setDataToViews();
+
+        initializePresenter();
 
         setupViewPager();
 
@@ -49,6 +73,32 @@ public class ImageGalleryActivity extends BaseActivity implements View.OnClickLi
 
         setListenerOnViews();
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        galleryPresenter.destroy();
+
+    }
+
+    private void injectActivityComponent() {
+
+
+        activityComponent = DaggerActivityComponent.builder()
+                .applicationComponent(getApplicationComponent())
+                .activityModule(new ActivityModule(this))
+                .build();
+
+        activityComponent.inject(this);
+    }
+
+    private void initializePresenter() {
+
+        galleryPresenter.init(this);
+    }
+
 
     private void setupViews() {
 
@@ -92,9 +142,11 @@ public class ImageGalleryActivity extends BaseActivity implements View.OnClickLi
 
     private void setupPagerAdapter() {
 
-        galleryPagerAdapter = new GalleryPagerAdapter(getLayoutInflater(), summaries);
+        galleryPagerAdapter = new GalleryPagerAdapter(getLayoutInflater(), summaries, this);
 
         galleryViewPager.setAdapter(galleryPagerAdapter);
+        
+        galleryViewPager.setCurrentItem(currentPosition);
 
     }
 
@@ -123,4 +175,39 @@ public class ImageGalleryActivity extends BaseActivity implements View.OnClickLi
 
         finish();
     }
+
+    //Gallery view methods.
+    //***********************
+
+    @Override
+    public void showError(String message) {
+
+        Toast.makeText(ImageGalleryActivity.this, message , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void loadImage(String imageURL, ImageView imageView) {
+
+        imageDownloader.downloadImage(imageURL, 500, 500, imageView);
+
+    }
+
+    @Override
+    public void showPlaceholderImage(ImageView imageView) {
+
+        imageView.setImageResource(R.mipmap.ic_launcher);
+
+    }
+
+    //********************
+    //Gallery view methods end.
+
+
+    @Override
+    public void loadImageData(String resourceURI, ImageView imageView) {
+
+        galleryPresenter.fetchImageData(resourceURI, new WeakReference<ImageView>(imageView));
+
+    }
+
 }
